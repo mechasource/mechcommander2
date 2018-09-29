92d2236b8529f0b6fc9de8d445e8ac8161cecc10
BeErikk <erikderoth-official@yahoo.se>
2018-09-29 17:00:45 +0200

    resource updates

--- source/mc2res/mc2version.h
+++ source/mc2res/mc2version.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright (c) 2011-2014, Jerker Back. All rights reserved.
+ Copyright (c) 2011-2014, Jerker Beck. All rights reserved.
 
  Permission to use, copy, modify, and distribute this software for any
  purpose with or without fee is hereby granted, provided that the following
@@ -28,7 +28,7 @@
 
  MechCommander 2 source code
 
- 2014-08-07 jerker_back, created
+ 2014-08-07 Jerker Beck, created
 
  $LastChangedBy$
 

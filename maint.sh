b2c7cecf001cec1f535aa8d29c31bdc30d9aa983
BeErikk <erikderoth-official@yahoo.se>
2020-07-19 14:51:29 +0200

    maintainance tools

--- maint.sh
+++ maint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # written for and tested in ubuntu WSL bash
+formatcode="/mnt/d/devapps/compilers/LLVM/bin/clang-format.exe -i -style=file"
+#formatcode="clang-format -i -style=file"
 
-find ./source -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.inl" -o -name "*.cpp" -o -name "*.c" \) -exec dos2unix -k -q {} \;
+
+find ./source -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.inl" -o -name "*.inc" -o -name "*.cpp" -o -name "*.c" -o -name "*.fx" -o -name "*.fxh" -o -name "*.hlsl" -o -name "*.hlsli" \) -exec dos2unix -k -q {} \;
 unix2dos -k -q source/mc2res/resource.h
-find ./source -type f -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -exec clang-format -i -style=file {} \;
+# find ./source -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.inl" -o -name "*.cpp" -o -name "*.c" \) -exec "$formatcode" {} \;
 
 # find ./source/mechcmd2 -depth -type f -name "*.hpp" -exec sh -c 'git mv "$1" "${1%.hpp}.h"' _ {} \; 
 # for /f "tokens=*" %G in ('@dir /s /b /o:gn "source\*.h"') DO @clang-format.exe -i -style=file "%G"
